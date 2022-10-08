d012e6fe35bd0be809f0b81c20509d88949bdf76
Shiv Srivastava <1983.shiv@gmail.com>
2022-10-08 08:39:29 +0530

    fdfd

--- app.py
+++ app.py
@@ -6,52 +6,52 @@ import json
 from bs4 import BeautifulSoup as bs
 from fastapi import FastAPI
 import uvicorn
-# import subprocess
+import subprocess
 
-# subprocess.run(["playwright", "install"])
-# subprocess.run(["playwright", "install-deps"])
+subprocess.run(["playwright", "install"])
+subprocess.run(["playwright", "install-deps"])
 
 
-# async def scrap():
-#     items = []
-#     async with async_playwright() as p:
-#         browser = await p.chromium.launch(headless=True)
-#         page = await browser.new_page()
-#         await page.goto("https://www.ergodotisi.com/en/SearchResults.aspx")
+async def scrap():
+    items = []
+    async with async_playwright() as p:
+        browser = await p.chromium.launch(headless=True)
+        page = await browser.new_page()
+        await page.goto("https://www.ergodotisi.com/en/SearchResults.aspx")
 
-#         i = 0
-#         # while i < 15:
-#         #     await page.mouse.wheel(0, 4000)
-#         #     await page.wait_for_timeout(3000)
-#         #     # page.wait_for_selector("text=More Job Posts...", 3000)
-#         #     await page.locator("text=More Job Posts...").click()
-#         #     time.sleep(4)
-#         #     i = i + 1
+        i = 0
+        # while i < 15:
+        #     await page.mouse.wheel(0, 4000)
+        #     await page.wait_for_timeout(3000)
+        #     # page.wait_for_selector("text=More Job Posts...", 3000)
+        #     await page.locator("text=More Job Posts...").click()
+        #     time.sleep(4)
+        #     i = i + 1
 
-#         time.sleep(3)
-#         html = await page.content()
+        time.sleep(3)
+        html = await page.content()
 
-#         soup = bs(html, 'html.parser')
-#         # print(soup.prettify())
-#         allListing = soup.find_all(
-#             'article', attrs={'class': 'search-result-card'})
-#         # print(len(allListing))
-#         for listing in allListing:
-#             if ("yesterday" in listing.find_all("p")[3].text):
-#                 # print(listing.find_all("p")[3].text)
-#                 item = {
-#                     "title": listing.find_all("a")[0].text,
-#                     "url": listing.find_all("a")[1].text,
-#                     "city": listing.find_all("p")[0].text,
-#                     "cmpy": listing.find_all("p")[1].text,
-#                     "view": listing.find_all("p")[2].text,
-#                     "date": listing.find_all("p")[3].text,
-#                     "expire": listing.find_all("p")[4].text,
-#                     "typeofjob": listing.find_all("p")[5].text
-#                 }
-#                 items.append(item)
-#         await browser.close()
-#     return items
+        soup = bs(html, 'html.parser')
+        # print(soup.prettify())
+        allListing = soup.find_all(
+            'article', attrs={'class': 'search-result-card'})
+        # print(len(allListing))
+        for listing in allListing:
+            if ("yesterday" in listing.find_all("p")[3].text):
+                # print(listing.find_all("p")[3].text)
+                item = {
+                    "title": listing.find_all("a")[0].text,
+                    "url": listing.find_all("a")[1].text,
+                    "city": listing.find_all("p")[0].text,
+                    "cmpy": listing.find_all("p")[1].text,
+                    "view": listing.find_all("p")[2].text,
+                    "date": listing.find_all("p")[3].text,
+                    "expire": listing.find_all("p")[4].text,
+                    "typeofjob": listing.find_all("p")[5].text
+                }
+                items.append(item)
+        await browser.close()
+    return items
 
 app = FastAPI()
 
